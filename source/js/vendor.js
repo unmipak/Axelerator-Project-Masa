64501236bb6e42049e5db01a3432222969d8b207
Маргарита Пак <paktoka@gmail.com>
2023-09-05 23:24:10 +0300

    добавляет стили

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,16 @@
 // Swiper 7.4.1
-import './vendor/swiper';
+import Swiper, {Navigation, Pagination} from './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
+Swiper.use([Navigation, Pagination]);
+
+const sliderHero = new Swiper('.hero', {
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
+  },
+});
+
+export {sliderHero};
